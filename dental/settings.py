aa2928b439240ae5da43d6ed9ecc595f635838b3
Herucara TAM <carlbas@hotmail.com>
2021-07-12 18:47:46 +0200

    tweak app for heroku

--- dental/settings.py
+++ dental/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-#import django_heroku
-#import dj_database_url
-#from decouple import config
+import django_heroku
+import dj_database_url
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddelware',
 ]
 
 ROOT_URLCONF = 'dental.urls'
@@ -127,6 +128,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'Dkwakuba100@gmail.com'
 EMAIL_HOST_PASSWORD = '55Coppo55'
@@ -169,5 +172,7 @@ EMAIL_USE_TLS = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+django_heroku.settings(locals())
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
